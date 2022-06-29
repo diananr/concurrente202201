f3b8d54238f0592ff73396852352f331686a2755
"Ernesto" <"nyxernesto2@gmail.com">
2022-06-28 19:45:13 -0500

    #add agregarTiempoFin, RetirarApuesta y validaciones de tiempo

--- backend/Auction/Subasta.sol
+++ backend/Auction/Subasta.sol
@@ -6,15 +6,20 @@ contract Subasta{
     mapping(address => uint) postoresData;
     uint apuestaMayor;
     address postorMayor;
+    uint tiempoInicio = block.timestamp;
+    uint tiempoFin;
+
+
 
     //Agregar una nueva apuesta
     function agregarApuesta() public payable{
         //Conficional que la apuesta no sea cero
         uint calcularMonto = postoresData[msg.sender]+msg.value;
         require(msg.value > 0, "La apuesta debe ser mayor a 0");
-
+        //Verificar que la subasta no expiró
+        require(block.timestamp<=tiempoFin, "La subasta ya termino");
         //Verificar Apuesta Mayor
-        require(calcularMonto > apuestaMayor, "Su monto está por debajo de la Apuesta Mayor");
+        require(calcularMonto > apuestaMayor, "Su monto esta por debajo de la Apuesta Mayor");
         postoresData[msg.sender] = calcularMonto;
         apuestaMayor = calcularMonto;
         postorMayor=msg.sender;
@@ -32,5 +37,17 @@ contract Subasta{
     //Obtener la dirección del apostador mayor
     function MayorPostorAddr() public view returns(address){
         return postorMayor;
+    }
+
+    //Hora que termina la Subasta
+    function crearTiempoFin(uint _tiempoFin) public {
+        tiempoFin = _tiempoFin;
+    }
+
+    //Retirar Apuesta
+    function RetirarApuesta(address payable _address) public{
+        if(postoresData[_address]>0){
+            _address.transfer(postoresData[_address]);
+        }
     }
 }
